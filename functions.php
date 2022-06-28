0260dd36573b6c65210ecc236b6e32f0954b46a4
Michael Wender <michael@michaelwender.com>
2022-06-28 15:06:12 -0400

    Adding `uber_log()`

--- functions.php
+++ functions.php
@@ -82,3 +82,22 @@ function add_rss_node_image() {
 }
 
 add_action('rss2_item', 'add_rss_node_image');
+
+/**
+ * Enhanced logging.
+ *
+ * @param      string  $message  The log message
+ */
+if( ! function_exists( 'uber_log'  ) ){
+  function uber_log( $message = null ){
+    static $counter = 1;
+
+    $bt = debug_backtrace();
+    $caller = array_shift( $bt );
+
+    if( 1 == $counter )
+      error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
+    error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
+    $counter++;
+  }
+}
