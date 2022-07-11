504207ca1d8fe61202d646ef1bbdb74e30f01145
Michael Wender <michael@michaelwender.com>
2022-07-11 12:31:26 -0400

    Adding ad to Latest Posts row

--- templates/template-parts/home-2-latest-posts.php
+++ templates/template-parts/home-2-latest-posts.php
@@ -1,32 +1,36 @@
 <?php
   //*
-  $latest_posts = get_posts( [ 'numberposts' => 3 ] );
+  $latest_posts = get_posts( [ 'numberposts' => 2 ] );
   if( $latest_posts ){
     ?>
     <div class="row">
-      <div class="col-lg-12">
+      <div class="col-lg-8">
         <div class="topper-wide">
             <h2>Latest Columns</h2>
         </div>
-      </div>
-    </div>
-    <div class="row">
-    <?php
-    foreach( $latest_posts as $post ):
-      setup_postdata( $post );
-      ?>
-      <div class="col-md-4">
-        <header>
-          <h4 class="article-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
-        </header>
-        <div class="post-excerpt">
-          <?php the_excerpt(); ?>
-        </div>
-      </div>
+        <div class="row articles">
+        <?php
+        foreach( $latest_posts as $post ):
+          setup_postdata( $post );
+          ?>
+          <div class="col-lg-6">
+            <header>
+              <h4 class="article-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
+            </header>
+            <div class="post-excerpt">
+              <?php the_excerpt(); ?>
+            </div>
+          </div>
 
-      <?php
-    endforeach;
-    ?></div><!-- .row --><?php
+          <?php
+        endforeach;
+        ?></div><!-- .row.articles -->
+      </div><!-- .col-lg-8 -->
+      <div class="col-lg-4">
+        <div id='rectangle_1'></div>
+      </div><!-- .col-lg-4 -->
+    </div><!-- .row -->
+<?php
     wp_reset_postdata();
   }
   /**/
